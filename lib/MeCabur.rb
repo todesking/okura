8524cec937b946e1ab9ddd0aeff75c92a8ff1caf
todesking <discommunicative@gmail.com>
2011-12-16 20:39:02 +0900

    hoge

--- lib/MeCabur.rb
+++ lib/MeCabur.rb
@@ -10,22 +10,47 @@ module MeCabur
     def initialize
       @dic=todo
     end
+    # -> [String]
     def wakati str
       parse(str).mincost_path.map{|node|node.word.surface}
     end
+    # -> Nodes
     def parse str
       chars=str.split(//)
       nodes=Nodes.new(chars.length+2)
-      nodes.add(0,mk_bos())
-      nodes.add(chars.length+1,mk_eos)
+      nodes.add(0,Node.mk_bos)
+      nodes.add(chars.length,Node.mk_eos)
       str.length.times{|i|
         @dic.possible_words(str,i).each{|w|
-          nodes.add(i+1,mk_node(w))
+          nodes.add(i,w)
         }
       }
       nodes
     end
-    private
+  end
+  class Nodes
+    def initialize len
+      @begins=todo
+      @ends=todo
+    end
+    todo
+    def last
+      self[-1]
+    end
+    def [](i)
+      @begins[i]
+    end
+    def mincost_path
+      last.each{|eos|
+      }
+      todo
+    end
+    def add i,word
+      todo
+    end
+  end
+  class Node
+    attr_reader :word
     def mk_eos
       todo
     end
@@ -33,22 +58,32 @@ module MeCabur
       todo
     end
     def mk_node
+      todo
     end
-  end
-  class Nodes
-    todo
-  end
-  class Node
-    attr_reader :word
   end
   class Word
     attr_reader :surface
   end
   class Dic
     todo
+    # -> [Word]
+    def possible_words str,i
+      @wdic.lookup(str,i)+@udic.possible_words(str,i)
+    end
+  end
+  class WordDic
+    # -> [Word]
     def possible_words str,i
       todo
     end
+    todo
+  end
+  class UnkDic
+    # -> [Word]
+    def possible_words str,i
+      todo
+    end
+    todo
   end
   class Matrix
     todo
